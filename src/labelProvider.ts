9f63c31dadfb540900140df7b20598a94d99d1d9
Maxence G. de Montauzan <maxence@gdemontauzan.fr>
2022-08-18 16:22:25 +0200

    Refacto: Abstract class for providers

--- src/labelProvider.ts
+++ src/labelProvider.ts
@@ -1,79 +1,39 @@
 import * as vscode from 'vscode';
 
-import { Config } from './config';
-import { GiteaConnector } from './giteaConnector';
-import { Issue } from './issue';
+import { AbstractProvider } from './abstractProvider';
 import { IssueProvider } from './issueProvider';
 import { Label } from './label';
 import { Logger } from './logger';
 
-export class LabelProvider implements vscode.TreeDataProvider<Label> {
-    private _onDidChangeTreeData: vscode.EventEmitter<Label | undefined | null | void> = new vscode.EventEmitter<Label | undefined | null | void>();
-
-    readonly onDidChangeTreeData: vscode.Event<Label | undefined | null | void> = this._onDidChangeTreeData.event;
-
-    private labelList: Label[] = [];
-
-    private readonly logger: Logger;
-
+export class LabelProvider extends AbstractProvider<Label> {
     constructor(logger: Logger) {
-        this.logger = logger;
+        super(logger)
     }
 
-    public getTreeItem(element: Label): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        return element;
-    }
+    public async getData(page: number = 1) : Promise<Label[]> {
+        let labels: Label[] = [];
+        this.logger.log( `Retrieve labels - page ${page}`);
+        const labelsOfPage = (await this.giteaConnector.getLabels(this.config.repoApiLabelsUrl, page)).data;
+        this.logger.log( `Get Labels page ${page}: ${labelsOfPage.length} retrieved`);
 
-    public async getLabels() {
-        this.labelList = [];
-        const config = new Config();
-        const giteaConnector = new GiteaConnector(config.token, config.sslVerify);
+        labelsOfPage.forEach((c) => {
+            c.label = c.name
+            let label = Label.createLabel(c)
 
-        const labels = [];
-        let page = 1;
-        while (page < 11) {
-            this.logger.log( `Retrieve labels - page ${page}`);
-            const labelsOfPage = (await giteaConnector.getLabels(config.repoApiLabelsUrl, page)).data;
-            this.logger.log( `Get Labels page ${page}: ${labelsOfPage.length} retrieved`);
-            labels.push(...labelsOfPage);
-            labelsOfPage.forEach((c) => {
-                c.label = c.name
-                c.labelId = c.id;
-            });
-            page++;
-            if (labelsOfPage.length < 10) {
-                break;
-            }
-        }
-        this.labelList = [];
-        labels.forEach((element: Label) => {
-            let label = Label.createLabel(element)
+            label.labelId = c.id;
             label.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
             label.contextValue = 'label';
             label.issueProvider = new IssueProvider('all', this.logger, label.name)
-            this.labelList.push(label)
+            labels.push(label)
         });
+
+        return labels
     }
 
-    public async refresh() {
-        await this.getLabels();
-        this._onDidChangeTreeData.fire();
-    }
-
-    public getChildren(element?: Label): vscode.ProviderResult<any[]> {
-        if (element === undefined) {
-            return this.labelList;
-        }
-
-        if (element instanceof Label) {
-            return this.createChildNodes(this.labelList, element)
-        }
-    }
-
-    private async createChildNodes(labels: Label[], element?: Label) {
-        for (const label of labels) {
+    protected async createChildNodes(element?: Label) {
+        for (const label of this.elementList) {
             if (element === label) {
-                let issues = await Promise.resolve(label.issueProvider?.getIssues());
+                let issues = await Promise.resolve(label.issueProvider?.getElements());
 
                 if (!issues) return Promise.resolve([]);
 
@@ -83,7 +43,7 @@ export class LabelProvider implements vscode.TreeDataProvider<Label> {
                 return Promise.resolve(childItems)
             }
         }
-        return this.labelList;
+        return this.elementList;
     }
 }
 
